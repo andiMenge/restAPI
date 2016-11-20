f059db86839fa621b5e7e79abfa231f97edbefd1
Andi <123fakeAdress@lulz.nope>
2016-11-20 02:30:19 +0100

    add cmdline flags

--- main.go
+++ main.go
@@ -1,19 +1,29 @@
 package main
 
 import (
+	"flag"
 	"net/http"
 )
 
-const port = ":1337"
+var (
+	apiPort   = flag.String("port", "1337", "defines the port for the webserver")
+	redisAddr = flag.String("redis", "localhost:6379", "addr of redis server")
+	redisPW   = flag.String("pw", "", "pw for redis server. \"\" for no pw") // no password set
+	redisDB   = flag.Int("db", 0, "defines redis db. 0 for default db")      // use default DB
+)
 
 func main() {
+	flag.Parse() //parse cmdline flags
+
+	//start redis client
 	err := initRedis()
 	if err != nil {
-		ErrLog.Fatalf("connection with redis failed: %s", err)
+		ErrLog.Fatalf("connection to redis failed: %s", err)
 	}
-	InfoLog.Printf("connection with redis successful")
+	InfoLog.Printf("connection to redis successful")
 
+	//start webserver
 	router := NewRouter()
-	InfoLog.Printf("starting webserver on port %s", port)
-	ErrLog.Fatal(http.ListenAndServe(port, router))
+	InfoLog.Printf("starting webserver on port 0.0.0.0:%s", *apiPort)
+	ErrLog.Fatal(http.ListenAndServe(":"+*apiPort, router))
 }
